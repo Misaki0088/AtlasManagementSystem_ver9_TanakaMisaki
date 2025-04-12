3495647ff3fa5fd6c637870c4a284dbba3048d9b
DESKTOP-A92J59S\misak <misaki.tanaka0827@gmail.com>
2025-04-12 14:28:38 +0900

    初期データ登録、「国語、数学、英語」のレコード作成完了

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -3,6 +3,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class UsersTableSeeder extends Seeder
 {
@@ -13,6 +14,16 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-
+        DB::table('users')->insert([
+            'over_name' => '田中',
+            'under_name' => '美沙希',
+            'over_name_kana' => 'タナカ',
+            'under_name_kana' => 'ミサキ',
+            'mail_address' => 'misaki.tanaka@tanaka.com',
+            'sex' => '2',
+            'birth_day' => '1991-08-27',
+            'role' => '2',
+            'password' =>bcrypt('00000000'),
+        ]);
     }
 }
