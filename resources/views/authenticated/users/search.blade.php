aa0aa19ee38412a60dd8bd8d00aeaf5e309a5c0e
DESKTOP-A92J59S\misak <misaki.tanaka0827@gmail.com>
2025-04-21 18:40:47 +0900

    一覧画面で選択科目が表示されるようになりました！

--- resources/views/authenticated/users/search.blade.php
+++ resources/views/authenticated/users/search.blade.php
@@ -43,7 +43,14 @@
       </div>
       <div>
         @if($user->role == 4)
-        <span>選択科目 :</span>
+          <span>選択科目 :</span>
+          @if($user->subjects->isNotEmpty())
+          @foreach($user->subjects as $subject)
+          {{ $subject->subject }}@if(!$loop->last)、@endif
+          @endforeach
+          @else
+          未選択
+          @endif
         @endif
       </div>
     </div>
