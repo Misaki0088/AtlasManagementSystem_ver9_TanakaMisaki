9bce21c358c97f2ceafdff6a94c13814c528ff3e
DESKTOP-A92J59S\misak <misaki.tanaka0827@gmail.com>
2025-05-17 19:13:15 +0900

    モーダルは表示できた。（中身はまだ…）

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -1,21 +1,3 @@
 $(function(){
-$('.open-cancel-modal').on('click', function () {
-$('.modal.fade').fadeIn();
-    var reserveDate = $(this).attr('data-reserve-date'); // 予約日
-    var reservePart = $(this).attr('data-reserve'); //部数
 
-
-  // モーダルに予約日と時間（部数）を表示
-  $('.reserveDate p').text(reserveDate);
-  $('.reservePart p').text(reservePart);
-return false;
-
-  // 予約IDを隠しフィールドにセット
-  // modal.find('#cancelReserveId').val(reserveId);
 });
-});
-// $(function () {
-
-// alert('ハローワールド');
-
-// });
--- resources/views/authenticated/calendar/general/calendar.blade.php
+++ resources/views/authenticated/calendar/general/calendar.blade.php
@@ -45,6 +45,25 @@
   </div>
 </div>
 
+{{-- 予約キャンセルモーダルのJS --}}
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <script>
+    $('#cancelModal').on('click', function () {
+      $('.modal.fade').fadeIn();
+      var reserveDate = $(this).attr('data-reserve-date'); // 予約日
+      var reservePart = $(this).attr('data-reserve'); //部数
+
+
+  // モーダルに予約日と時間（部数）を表示
+  $('.reserveDate p').text(reserveDate);
+  $('.reservePart p').text(reservePart);
+return false;
+
+  // 予約IDを隠しフィールドにセット
+  // modal.find('#cancelReserveId').val(reserveId);
+});
+  </script>
 
 
 </x-sidebar>
